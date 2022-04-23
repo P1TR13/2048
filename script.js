e55e276712327a6a0c7ac11834da49fd8dabb3ef
P1TR13 <pawel.michalowski@onet.pl>
2022-04-23 14:15:22 +0200

    Making random number on random place

--- script.js
+++ script.js
@@ -9,11 +9,21 @@ let positions = {
 
 class block {
     constructor(number) {
-        this.number = number;
+        this.number = Math.pow(2, number);
+    }
 
-        this.createBlock = function (squareID) {
-            $('#square' + squareID).append('<div class = \'block n' + this.number + '\'>' + this.number + '</div>')
-        }
+    createBlock(squareID) {
+        $('#square' + squareID).append('<div class = \'block n' + this.number + '\'>' + this.number + '</div>')
+    }
+
+    moveUp(direction) {
+        if (direction) console.log("Move up");
+        else console.log("Move down");
+    }
+
+    moveRight(direction) {
+        if (direction) console.log("Move right");
+        else console.log("Move left");
     }
 }
 
@@ -32,7 +42,6 @@ function initialPosition() {
         positions.lastPosition[i] = 0;
         positions.ifItemsWereAdded[i] = 0;
     }
-    console.log(positions);
 
     currentScore = 0;
     $('#currentScore').html(currentScore);
@@ -40,8 +49,46 @@ function initialPosition() {
 }
 
 function makeBlock() {
-    let newBlock = new block(2);
-    newBlock.createBlock(4);
+    let newBlock = new block(gettingRandomNumber(1, 2));
+    newBlock.createBlock(gettingRandomNumber(0, 16));
+    addMovingToBlock(newBlock);
+}
+
+function addMovingToBlock(block) {
+    $('body').keyup(function(event) {
+        let key = event.key;
+        console.log(key);
+        switch(key) {
+            case "ArrowUp":
+                block.moveUp(1);
+                break;
+            case "w":
+                block.moveUp(1);
+                break;
+            case "ArrowDown":
+                block.moveUp(-1);
+                break;
+            case "s":
+                block.moveUp(-1);
+                break;
+            case "ArrowRight":
+                block.moveRight(1);
+                break;
+            case "d":
+                block.moveRight(1);
+                break;
+            case "ArrowLeft":
+                block.moveRight(-1);
+                break;
+            case "a":
+                block.moveRight(-1);
+                break;
+        }
+    });
+}
+
+function gettingRandomNumber(from, to) {
+    return Math.floor(from + Math.random() * to);
 }
 
 
