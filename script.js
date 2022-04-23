1a23b38eb5d00af5f69e1e9564830e2530956c36
P1TR13 <pawel.michalowski@onet.pl>
2022-04-23 13:37:13 +0200

    Added block class (creating new squares)

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const boardSize = 16;
+let currentScore = 0;
 
 let positions = {
     currentPosition: [],
@@ -6,22 +7,43 @@ let positions = {
     ifItemsWereAdded: [],
 };
 
-function makeBoardWithEmptySquares() {
-    for (let i = 0; i < boardSize; i++) {
-        $('#board').append('<div class = \'square\'> </div>')
+class block {
+    constructor(number) {
+        this.number = number;
+
+        this.createBlock = function (squareID) {
+            $('#square' + squareID).append('<div class = \'block n' + this.number + '\'>' + this.number + '</div>')
+        }
     }
-    makeEmptyPositions();
 }
 
-function makeEmptyPositions() {
+function makeBoardWithEmptySquares() {
+    for (let i = 0; i < boardSize; i++) {
+        $('#board').append('<div class = \'square\' id = \'square' + i + '\'> </div>')
+    }
+    initialPosition();
+    $('#newGameButton').click(initialPosition);
+    makeBlock();
+}
+
+function initialPosition() {
     for (let i = 0; i < boardSize; i++) {
         positions.currentPosition[i] = 0;
         positions.lastPosition[i] = 0;
         positions.ifItemsWereAdded[i] = 0;
     }
-
     console.log(positions);
+
+    currentScore = 0;
+    $('#currentScore').html(currentScore);
+    $('#newGameButton').html('New Game');
 }
+
+function makeBlock() {
+    let newBlock = new block(2);
+    newBlock.createBlock(4);
+}
+
 
 // On Load
 makeBoardWithEmptySquares();
